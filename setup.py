14d9f43ec1fe1ad101c0e02a4835cff67e10a597
Chris Loew <cloew123@gmail.com>
2014-07-13 14:16:49 -0600

    Added package core

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='pbf_toggl',
+      version='.1',
+      description="Programmer's Best Friend Utility Extension for Toggl",
+      author='Chris Loew',
+      author_email='cloew123@gmail.com',
+      packages=['pbf_toggl', 'pbf_toggl.Commands', 'pbf_toggl.templates'],
+      #package_data = {'pbf_toggl.templates':[]}, # Add template files
+     )
