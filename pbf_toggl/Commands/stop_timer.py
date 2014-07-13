b474c321946230aae7752c3efec37f70eea0b5b8
Chris Loew <cloew123@gmail.com>
2014-07-13 15:03:10 -0600

    Added stop timer command

---
+++ pbf_toggl/Commands/stop_timer.py
@@ -0,0 +1,27 @@
+from pbf.Commands import command_manager
+
+from pbf_toggl.helpers.toggl_project_helper import GetCurrentTogglProject
+
+class StopTimerCommand:
+    """ Represents command to stop the current timer """
+    category = "stop"
+    command = "timer"
+    description = "Stop the current timer for Toggl"
+    minimumNumberOfArguments = 0
+    
+    def run(self, args):
+        """ Run the command """
+        self.stopTimer()
+        
+    def stopTimer(self):
+        """ Stop the Toggl Timer """
+        pbfProject = GetCurrentTogglProject()
+        timeEntry = pbfProject.togglAPI.timer.current()
+        pbfProject.togglAPI.timer.stopTimer(timeEntry)
+    
+    def help(self):
+        """ Print Command usage """
+        print "Usage: pbf {category} {command}".format(category=self.category, command=self.command)
+        print "Stop the current Toggl Timer"
+    
+command_manager.RegisterCommand(StopTimerCommand)
