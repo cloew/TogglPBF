500a83cc5541442635845c9d078831a66d5b9dfa
Chris Loew <cloew123@gmail.com>
2014-08-23 12:25:05 -0600

    Can now specify the workspace to use for creating a new project

--- pbf_toggl/Commands/new_toggl_project.py
+++ pbf_toggl/Commands/new_toggl_project.py
@@ -17,10 +17,12 @@ class NewTogglProject:
         parser.add_argument('projectname', action='store', help='Toggl Project Name to connect to')
         parser.add_argument('connection', action='store', nargs='?', default='default', help='Toggl Connection to connect to')
         parser.add_argument('-s', '--store', action='store_true', help='Store this Toggl Project as the connection for this PBF Project')
+        parser.add_argument('-w', '--workspace', action='store', help='The Workspace to add the project to')
     
     def run(self, arguments):
         """ Run the command """
         projectName = arguments.projectname
+        workspaceName = arguments.workspace
         togglConnection = arguments.connection
             
         pbfProject = None
@@ -30,16 +32,25 @@ class NewTogglProject:
                 print "No PBF project for current directory"
         
         if not arguments.store or pbfProject is not None:
-            self.createTogglProject(projectName, togglConnection, pbfProject=pbfProject)
+            self.createTogglProject(projectName, togglConnection, pbfProject=pbfProject, workspaceName=workspaceName)
         
-    def createTogglProject(self, projectName, togglConnection, pbfProject=None):
+    def createTogglProject(self, projectName, togglConnection, pbfProject=None, workspaceName=None):
         """ Create the Toggl Project """
-        togglProject = pytoggl.Project(name=projectName)
         toggl = togglSettings.GetTogglAPI(togglConnection)
+        workspace = toggl.workspaces.findByName(workspaceName)
+        togglProject = self.buildTogglProject(projectName, workspace=workspace)
+        
         toggl.projects.create(togglProject)
         
         if pbfProject is not None:
             self.storeProjectSettings(projectName, togglConnection, pbfProject)
+        
+    def buildTogglProject(self, projectName, workspace=None):
+        """ Build the Toggl Project """
+        togglProject = pytoggl.Project(name=projectName)
+        if workspace is not None:
+            togglProject.wid = workspace.id
+        return togglProject
         
     def storeProjectSettings(self, projectName, togglConnection, pbfProject):
         """ Store the Project Settings """
